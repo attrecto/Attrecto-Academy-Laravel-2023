232a5c75de465538d414fec5b7e40a3eb3d1684e
mate.egyed <mate.egyed@attrecto.com>
2023-03-29 09:38:41 +0200

    lesson 4

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +21,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/users',[UserController::class,'store']);
+
 Route::post('/courses',[CourseController::class,'store']);
 Route::get('/courses',  [CourseController::class, 'index']);
 Route::get('/courses/{id}',  [CourseController::class, 'show']);
 
 Route::put('/courses/{id}',[CourseController::class,'update']);
 Route::delete('/courses/{id}',[CourseController::class,'destroy']);
+
+
+Route::post('/auth/login',[AuthController::class,'login']);
+
+Route::group(['middleware' => ['api']], function() {
+    Route::get('/users/me', [AuthController::class, 'me']);
+});
